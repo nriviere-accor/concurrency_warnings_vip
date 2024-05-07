c0111183017752e61a46c006e9d553639d1c41e1
Nicolas RIVIÈRE <nicolas.riviere@worldline.com>
2024-05-07 19:34:34 +0200

    Initial Commit

---
+++ TestActors/TestActorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestActorsApp.swift
+//  TestActors
+//
+//  Created by Nicolas RIVIERE on 07/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct TestActorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
